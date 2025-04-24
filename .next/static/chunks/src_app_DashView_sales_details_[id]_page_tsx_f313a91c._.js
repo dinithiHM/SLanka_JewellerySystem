e2dfe99537421dffd58046c3cd658820324f3564
dinithiHM <dinithihimesha29@gmail.com>
2025-04-24 14:40:09 +0530

    Updated Report UI

---
+++ .next/static/chunks/src_app_DashView_sales_details_[id]_page_tsx_f313a91c._.js
@@ -0,0 +1,11 @@
+(globalThis.TURBOPACK = globalThis.TURBOPACK || []).push([
+    "static/chunks/src_app_DashView_sales_details_[id]_page_tsx_f313a91c._.js",
+    {},
+]);
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+  "path": "static/chunks/src_app_DashView_sales_details_[id]_page_tsx_f313a91c._.js",
+  "chunks": [
+    "static/chunks/_4fab4252._.js"
+  ],
+  "source": "dynamic"
+});
