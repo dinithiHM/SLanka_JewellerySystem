8a30867cb748dfe6d50036858d6c0d83b2fd2b10
dinithiHM <dinithihimesha29@gmail.com>
2025-05-24 22:13:57 +0530

    make real names and images in landing page

--- src/data.ts
+++ src/data.ts
@@ -3,7 +3,7 @@ type Product = {
   title: string;
   desc?: string;
   img?: string;
-  price: number;
+  price?: number;
   options?: { title: string; additionalPrice: number }[];
 };
 
@@ -14,380 +14,88 @@ export const featuredProducts: Products = [
     id: 1,
     title: "Wedding Earings",
     desc: "Step into the spotlight with earrings that steal the show! ✨🎉 Perfectly designed for every unforgettable party moment",
-    img: "/placeholder.jpg",
-    price: 24.9,
-    options: [
-      {
-        title: "Small",
-        additionalPrice: 0,
-      },
-      {
-        title: "Medium",
-        additionalPrice: 4,
-      },
-      {
-        title: "Large",
-        additionalPrice: 6,
-      },
-    ],
+    img: "/8.jpg",
   },
   {
     id: 2,
-    title: "Wedding Earings",
+    title: "Barath Necklace",
     desc: "Step into the spotlight with earrings that steal the show! ✨🎉 Perfectly designed for every unforgettable party moment",
-    img: "/placeholder.jpg",
-    price: 29.9,
-    options: [
-      {
-        title: "Small",
-        additionalPrice: 0,
-      },
-      {
-        title: "Medium",
-        additionalPrice: 4,
-      },
-      {
-        title: "Large",
-        additionalPrice: 6,
-      },
-    ],
+    img: "/11.jpg",
   },
   {
     id: 3,
-    title: "Wedding Earings",
+    title: "Cluster Earings",
     desc: "Step into the spotlight with earrings that steal the show! ✨🎉 Perfectly designed for every unforgettable party moment",
-    img: "/placeholder.jpg",
-    price: 24.9,
-    options: [
-      {
-        title: "Small",
-        additionalPrice: 0,
-      },
-      {
-        title: "Medium",
-        additionalPrice: 4,
-      },
-      {
-        title: "Large",
-        additionalPrice: 6,
-      },
-    ],
+    img: "/25.jpg",
   },
   {
     id: 4,
     title: "Wedding Earings",
     desc: "Step into the spotlight with earrings that steal the show! ✨🎉 Perfectly designed for every unforgettable party moment",
-    img: "/placeholder.jpg",
-    price: 26.9,
-    options: [
-      {
-        title: "Small",
-        additionalPrice: 0,
-      },
-      {
-        title: "Medium",
-        additionalPrice: 4,
-      },
-      {
-        title: "Large",
-        additionalPrice: 6,
-      },
-    ],
+    img: "/47.jpg",
   },
   {
     id: 5,
     title: "Wedding Earings",
     desc: "Step into the spotlight with earrings that steal the show! ✨🎉 Perfectly designed for every unforgettable party moment",
-    img: "/placeholder.jpg",
-    price: 29.9,
-    options: [
-      {
-        title: "Small",
-        additionalPrice: 0,
-      },
-      {
-        title: "Medium",
-        additionalPrice: 4,
-      },
-      {
-        title: "Large",
-        additionalPrice: 6,
-      },
-    ],
+    img: "/70.jpg",
   },
   {
     id: 6,
     title: "Wedding Earings",
     desc: "Two hearts, one promise, and a perfect pair of rings to celebrate your forever 💍✨ Crafted for love, designed to shine.",
-    img: "/placeholder.jpg",
-    price: 24.9,
-    options: [
-      {
-        title: "Small",
-        additionalPrice: 0,
-      },
-      {
-        title: "Medium",
-        additionalPrice: 4,
-      },
-      {
-        title: "Large",
-        additionalPrice: 6,
-      },
-    ],
+    img: "/71.jpg",
   },
   {
     id: 7,
     title: "Wedding Earings",
     desc: "Step into the spotlight with earrings that steal the show! ✨🎉 Perfectly designed for every unforgettable party moment",
-    img: "/placeholder.jpg",
-    price: 28.9,
-    options: [
-      {
-        title: "Small",
-        additionalPrice: 0,
-      },
-      {
-        title: "Medium",
-        additionalPrice: 4,
-      },
-      {
-        title: "Large",
-        additionalPrice: 6,
-      },
-    ],
+    img: "/rings.jpg",
   },
-  {
-    id: 8,
-    title: "Wedding Earings",
-    desc: "Step into the spotlight with earrings that steal the show! ✨🎉 Perfectly designed for every unforgettable party moment",
-    img: "/placeholder.jpg",
-    price: 32.9,
-    options: [
-      {
-        title: "Small",
-        additionalPrice: 0,
-      },
-      {
-        title: "Medium",
-        additionalPrice: 4,
-      },
-      {
-        title: "Large",
-        additionalPrice: 6,
-      },
-    ],
-  },
-  {
-    id: 9,
-    title: "Wedding Earings",
-    desc: "Step into the spotlight with earrings that steal the show! ✨🎉 Perfectly designed for every unforgettable party moment",
-    img: "/placeholder.jpg",
-    price: 29.9,
-    options: [
-      {
-        title: "Small",
-        additionalPrice: 0,
-      },
-      {
-        title: "Medium",
-        additionalPrice: 4,
-      },
-      {
-        title: "Large",
-        additionalPrice: 6,
-      },
-    ],
-  },
+  // {
+  //   id: 8,
+  //   title: "Wedding Earings",
+  //   desc: "Step into the spotlight with earrings that steal the show! ✨🎉 Perfectly designed for every unforgettable party moment",
+  //   img: "/placeholder.jpg",
+  //   price: 32.9,
+  //   options: [
+  //     {
+  //       title: "Small",
+  //       additionalPrice: 0,
+  //     },
+  //     {
+  //       title: "Medium",
+  //       additionalPrice: 4,
+  //     },
+  //     {
+  //       title: "Large",
+  //       additionalPrice: 6,
+  //     },
+  //   ],
+  // },
+  // {
+  //   id: 9,
+  //   title: "Wedding Earings",
+  //   desc: "Step into the spotlight with earrings that steal the show! ✨🎉 Perfectly designed for every unforgettable party moment",
+  //   img: "/placeholder.jpg",
+  //   price: 29.9,
+  //   options: [
+  //     {
+  //       title: "Small",
+  //       additionalPrice: 0,
+  //     },
+  //     {
+  //       title: "Medium",
+  //       additionalPrice: 4,
+  //     },
+  //     {
+  //       title: "Large",
+  //       additionalPrice: 6,
+  //     },
+  //   ],
+  // },
 ];
 
 
 
 
-export const pizzas: Products = [
-  {
-    id: 1,
-    title: "Sicilian",
-    desc: "Ignite your taste buds with a fiery combination of spicy pepperoni, jalapeños, crushed red pepper flakes, and melted mozzarella cheese, delivering a kick with every bite.",
-    img: "/placeholder.jpg",
-    price: 24.9,
-    options: [
-      {
-        title: "Small",
-        additionalPrice: 0,
-      },
-      {
-        title: "Medium",
-        additionalPrice: 4,
-      },
-      {
-        title: "Large",
-        additionalPrice: 6,
-      },
-    ],
-  },
-  {
-    id: 2,
-    title: "Mediterranean Delight",
-    desc: "Embark on a culinary journey with this Mediterranean-inspired creation, featuring zesty feta cheese, Kalamata olives, sun-dried tomatoes, and a sprinkle of oregano.",
-    img: "/placeholder.jpg",
-    price: 32.9,
-    options: [
-      {
-        title: "Small",
-        additionalPrice: 0,
-      },
-      {
-        title: "Medium",
-        additionalPrice: 4,
-      },
-      {
-        title: "Large",
-        additionalPrice: 6,
-      },
-    ],
-  },
-  {
-    id: 3,
-    title: "Bella Napoli",
-    desc: "A classic Italian delight featuring a thin, crispy crust, tangy tomato sauce, fresh mozzarella, and a medley of aromatic herbs topped with lettuce, tomatoes, and a dollop of tangy mayo.",
-    img: "/placeholder.jpg",
-    price: 26.9,
-    options: [
-      {
-        title: "Small",
-        additionalPrice: 0,
-      },
-      {
-        title: "Medium",
-        additionalPrice: 4,
-      },
-      {
-        title: "Large",
-        additionalPrice: 6,
-      },
-    ],
-  },
-  {
-    id: 4,
-    title: "Pesto Primavera",
-    desc: "A classic Italian delight featuring a thin, crispy crust, tangy tomato sauce, fresh mozzarella, and a medley of aromatic herbs topped with lettuce, tomatoes, and a dollop of tangy mayo.",
-    img: "/placeholder.jpg",
-    price: 28.9,
-    options: [
-      {
-        title: "Small",
-        additionalPrice: 0,
-      },
-      {
-        title: "Medium",
-        additionalPrice: 4,
-      },
-      {
-        title: "Large",
-        additionalPrice: 6,
-      },
-    ],
-  },
-  {
-    id: 5,
-    title: "Veggie Supreme",
-    desc: "A classic Italian delight featuring a thin, crispy crust, tangy tomato sauce, fresh mozzarella, and a medley of aromatic herbs topped with lettuce, tomatoes, and a dollop of tangy mayo.",
-    img: "/placeholder.jpg",
-    price: 24.9,
-    options: [
-      {
-        title: "Small",
-        additionalPrice: 0,
-      },
-      {
-        title: "Medium",
-        additionalPrice: 4,
-      },
-      {
-        title: "Large",
-        additionalPrice: 6,
-      },
-    ],
-  },
-  {
-    id: 6,
-    title: "Four Cheese Fantasy",
-    desc: "Experience pure cheesy bliss with a melty blend of mozzarella, cheddar, provolone, and Parmesan cheeses, creating a rich and indulgent pizza experience.",
-    img: "/placeholder.jpg",
-    price: 22.9,
-    options: [
-      {
-        title: "Small",
-        additionalPrice: 0,
-      },
-      {
-        title: "Medium",
-        additionalPrice: 4,
-      },
-      {
-        title: "Large",
-        additionalPrice: 6,
-      },
-    ],
-  },
-];
-
-export const singleProduct: Product = {
-  id: 1,
-  title: "Sicilian",
-  desc: "Ignite your taste buds with a fiery combination of spicy pepperoni, jalapeños, crushed red pepper flakes, and melted mozzarella cheese, delivering a kick with every bite.",
-  img: "/placeholder.jpg",
-  price: 24.9,
-  options: [
-    {
-      title: "Small",
-      additionalPrice: 0,
-    },
-    {
-      title: "Medium",
-      additionalPrice: 4,
-    },
-    {
-      title: "Large",
-      additionalPrice: 6,
-    },
-  ],
-};
-
-
-type Menu = {
-  id: number;
-  slug: string;
-  title: string;
-  desc?: string;
-  img?: string;
-  color: string;
-}[];
-
-export const menu: Menu = [
-  {
-    id: 1,
-    slug: "pastas",
-    title: "Italian Pastas",
-    desc: "Savor the taste of perfection with our exquisite Italian handmade pasta menu.",
-    img: "/placeholder.jpg",
-    color: "white",
-  },
-  {
-    id: 2,
-    slug: "burgers",
-    title: "Juicy Burgers",
-    desc: "Burger Bliss: Juicy patties, bold flavors, and gourmet toppings galore.",
-    img: "/placeholder.jpg",
-    color: "black",
-  },
-  {
-    id: 3,
-    slug: "pizzas",
-    title: "Cheesy Pizzas",
-    desc: "Pizza Paradise: Irresistible slices, mouthwatering toppings, and cheesy perfection.",
-    img: "/placeholder.jpg",
-    color: "white",
-  },
-];
